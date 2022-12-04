825e11377b0141dc59182e350390d9d6753d75bb
Milan Vodák <milanvodak02@gmail.com>
2022-12-04 19:27:52 +0100

    Day 4, part 2

--- 04/04.c
+++ 04/04.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define MIN(a, b) a < b ? a : b
+#define MAX(a, b) a > b ? a : b
+
 struct area {
   int start;
   int end;
@@ -17,16 +20,29 @@ int contains(struct area *outer, struct area *inner)
   return 1;
 }
 
+/*
+ * Checks whether two areas have overlapping sections.
+ * @return 1 if the areas overlap, 0 otherwise
+ */
+int overlap(struct area *a1, struct area *a2)
+{
+  return (MAX(a1->start, a2->start)) <= (MIN(a1->end, a2->end));
+}
+
 int main()
 {
   FILE *file = fopen("input.txt", "r");
 
-  int count = 0;
+  int count1 = 0, count2 = 0;
   struct area a1, a2;
 
-  while (fscanf(file, "%d-%d,%d-%d\n", &a1.start, &a1.end, &a2.start, &a2.end) != EOF)
+  while (fscanf(file, "%d-%d,%d-%d\n", &a1.start, &a1.end, &a2.start, &a2.end) != EOF) {
     if (contains(&a1, &a2) || contains(&a2, &a1))
-      count++;
+      count1++;
+    if (overlap(&a1, &a2))
+      count2++;
+  }
 
-  printf("Count: %d\n", count);
+  printf("Subsets: %d\n", count1);
+  printf("Overlapping: %d\n", count2);
 }
