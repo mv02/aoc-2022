a67488b36c856e9b325ece0160ece0b688d65baf
Milan Vodák <milanvodak02@gmail.com>
2022-12-06 22:04:03 +0100

    Day 6, part 2

--- 06/06.c
+++ 06/06.c
@@ -4,16 +4,16 @@
 
 #define MAX_INPUT_LEN 5000
 
-int get_marker_position(char *data)
+int get_marker_position(char *data, int marker_len)
 {
   int len = strlen(data);
   int i, j, k;
 
-  for (i = 0; i <= len - 4; ++i) {
+  for (i = 0; i <= len - marker_len; ++i) {
     int duplicate = 0;
 
-    for (j = i; j < i + 3; ++j) {
-      for (k = j + 1; k < i + 4; ++k)
+    for (j = i; j < i + marker_len - 1; ++j) {
+      for (k = j + 1; k < i + marker_len; ++k)
         if (data[j] == data[k]) {
           duplicate = 1;
           break;
@@ -35,7 +35,8 @@ int main()
   char *input = malloc(MAX_INPUT_LEN);
   fscanf(file, "%s\n", input);
 
-  printf("Packet start marker: %d\n", get_marker_position(input));
+  printf("Packet start marker: %d\n", get_marker_position(input, 4));
+  printf("Message start marker: %d\n", get_marker_position(input, 14));
 
   free(input);
   fclose(file);
